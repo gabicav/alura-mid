0592c8da1951eab9e594c9690fed4cd31d8b276d
gabicav <148063809+gabicav@users.noreply.github.com>
2023-11-27 10:07:10 -0300

    Update main.js

--- aluramidi-curso-aula2/main.js
+++ aluramidi-curso-aula2/main.js
@@ -1,5 +1,23 @@
-function tocaSomPom () {
-    document.querySelector('#som_tecla_pom').play();
+function tocaSomPom (id Elemento Audio) {
+    const elemento = document.querySelector(id Elemento Audio);
+}
+const listadeTeclas = document.querySelectorAll('.tecla');
+let contador = 0;
+for(let contador = 0; contador<listadeTeclas.lenght; contador++){
+    const tecla = listaDeTeclas[contador];
+    const instrumento = tecla.classList[1];
+    const idAudio = `#som_$(instrumento)`; //tamplate string
+
+    tecla.onclick = function() {
+        tocaSom(idAudio);
+    }
+    tecla.onkeydown = function (evento){
+        if(evento.code === 'Enter' || evento.code === 'space'){
+        tecla.classlist.add('ativa');
+        }
+    }
+    tecla.onkeyup = function (){
+        tecla.classlist.remove('ativa')
+    }
 }
 
-document.querySelector('.tecla_pom').onclick = tocaSomPom;
